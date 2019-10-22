c73d0dd2171c84d0b6a548ae301a25fe569ba083
Brian Folts <brian.folts@gmail.com>
2019-10-22 16:45:35 -0700

    Add support for comments, change name to , link report with comment node.

--- tests/index.js
+++ tests/index.js
@@ -1,14 +1,26 @@
 
-var rule = require("../lib/index").rules["tsdoc-comments"];
+var rule = require("../lib/index").rules["syntax"];
 var RuleTester = require("eslint").RuleTester;
 
-var ruleTester = new RuleTester();
-ruleTester.run("tsdoc-comments", rule, {
-    valid: ["/**\nA great function!\n */\nfunction foobar() {}\n"],
+var ruleTester = new RuleTester({
+    env: {
+        es6: true
+    }
+});
+ruleTester.run("syntax", rule, {
+    valid: [
+        "/**\nA great function!\n */\nfunction foobar() {}\n",
+        "/**\nA great class!\n */\nclass FooBar {}\n"
+    ],
     invalid: [{
         code: "/**\n * This `is wrong\n */\nfunction foobar() {}\n",
         errors: [{
             messageId: "tsdoc-code-span-missing-delimiter"
-        }]   
+        }]
+    }, {
+        code: "/**\n * This `is wrong\n */\nclass FooBar {}\n",
+        errors: [{
+            messageId: "tsdoc-code-span-missing-delimiter"
+        }]
     }]
 });
